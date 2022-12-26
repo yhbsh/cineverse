1cfe18622f8cb5c0a36b51f79d18dfcbabd2e1dd
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2022-12-26 13:11:55 +0300

    separate all movies grid from the view

--- lib/features/movie/presentation/view/all_movies.dart
+++ lib/features/movie/presentation/view/all_movies.dart
@@ -2,70 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../../core/presentation/provider/stream/network_provider.dart';
+import '../../../../core/presentation/view/no_internet_connection.dart';
 import '../../../../core/presentation/widget/circular_indicator.dart';
 import '../constant/movies_enum.dart';
-import '../provider/future/fetch_movies.dart';
-import '../provider/notifier/movie_genre_notifier.dart';
-import '../widget/movie/movie_card.dart';
-
-int _getCrossAxisCount(double width) {
-  if (width < 600) return 2;
-  if (width < 900) return 3;
-  if (width < 1200) return 4;
-  if (width < 1500) return 5;
-  return 6;
-}
+import '../widget/all_movies/all_movies_grid.dart';
 
 class AllMoviesView extends HookConsumerWidget {
   const AllMoviesView({super.key, required this.type});
 
   final MoviesType type;
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final size = MediaQuery.of(context).size;
-    final isConnectedAsyncValue = ref.watch(isConnectedProvider);
-    final moviesAsyncValue = ref.read(fetchMoviesProvider(type: type, page: 1, genre: ref.watch(genreNotifierProvider)));
-
+  Widget _contentView(WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: Text(type.name), centerTitle: true, elevation: 0),
-      body: isConnectedAsyncValue.maybeWhen(
-        orElse: () => const CircularIndicator(),
-        data: (isConnected) {
-          if (!isConnected) return const Center(child: Text('No internet connection'));
-          return moviesAsyncValue.maybeWhen(
-            orElse: () => Container(),
-            data: (moviesEntity) {
-              return GridView.custom(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: _getCrossAxisCount(size.width),
-                  crossAxisSpacing: size.width * 0.01,
-                  mainAxisSpacing: size.height * 0.01,
-                  childAspectRatio: 0.67,
-                ),
-                childrenDelegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    final pageLimit = moviesEntity.movies.length;
-                    if (pageLimit == 0) return Container(alignment: Alignment.center, child: const Text('No movies found'));
+      body: AllMoviesGridView(type: type),
+    );
+  }
 
-                    final page = index ~/ pageLimit + 1;
-                    final itemIndexInPage = index % pageLimit;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isConnectedAsyncValue = ref.watch(isConnectedProvider);
 
-                    final moviesAsyncValue = ref.read(fetchMoviesProvider(type: type, page: page, genre: ref.watch(genreNotifierProvider)));
+    return isConnectedAsyncValue.maybeWhen(
+      orElse: () => const CircularIndicator(),
+      data: (isConnected) {
+        if (!isConnected) return const NoInternetConnectionView();
 
-                    return moviesAsyncValue.maybeWhen(
-                      orElse: () => null,
-                      data: (moviesEntity) => MovieCard(movie: moviesEntity.movies[itemIndexInPage]),
-                    );
-                  },
-                  addAutomaticKeepAlives: false,
-                  childCount: moviesEntity.totalResults,
-                ),
-              );
-            },
-          );
-        },
-      ),
+        return _contentView(ref);
+      },
     );
   }
 }
