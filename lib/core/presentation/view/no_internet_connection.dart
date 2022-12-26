6176b9e73d4764f19a7e1904ed848be2fc114adf
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2022-12-26 13:00:30 +0300

    Create No Internet Connection view

---
+++ lib/core/presentation/view/no_internet_connection.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../resource/app_color.dart';
+
+class NoInternetConnectionView extends HookConsumerWidget {
+  const NoInternetConnectionView({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Center(
+      child: Text(
+        'No internet connection',
+        style: TextStyle(fontSize: 22, color: AppColor.white, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
