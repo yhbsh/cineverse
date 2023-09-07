8b54805f6e2523a67395f303b9ede5526702f1bc
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-09-07 15:45:31 +0300

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,7 @@ import 'lib.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await SystemChrome.setPreferredOrientations(
-    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
-  );
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
   runApp(
     ProviderScope(
