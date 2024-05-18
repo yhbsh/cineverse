c26e148531a817a9c07c8c6c72470c8738792f52
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-05-18 09:00:30 +0300

    add movie screen & navigate to it

---
+++ lib/screens/movie_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatelessWidget {
+  const MovieScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Hello World")),
+    );
+  }
+}
--- lib/widgets/movies_grid_entry.dart
+++ lib/widgets/movies_grid_entry.dart
@@ -1,3 +1,4 @@
+import 'package:cineverse/screens/movie_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../controllers/movies_controller.dart';
@@ -9,20 +10,29 @@ class MoviesGridEntry extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      fit: BoxFit.cover,
-      movie.posterURL,
-      loadingBuilder: (context, child, loadingProgress) {
-        if (loadingProgress == null) return child;
-
-        return const Align(child: CircularProgressIndicator.adaptive());
-      },
-      frameBuilder: (_, child, __, ___) {
-        return ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(8)),
-          child: child,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const MovieScreen(),
+          ),
         );
       },
+      child: Image.network(
+        fit: BoxFit.cover,
+        movie.posterURL,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+
+          return const Align(child: CircularProgressIndicator.adaptive());
+        },
+        frameBuilder: (_, child, __, ___) {
+          return ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            child: child,
+          );
+        },
+      ),
     );
   }
 }
