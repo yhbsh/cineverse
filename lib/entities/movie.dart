2224555f9f1df1bb18d5c5dabbb696646ac5011d
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-05-31 20:29:09 +0300

    add pageURL to movie

--- lib/entities/movie.dart
+++ lib/entities/movie.dart
@@ -4,6 +4,7 @@ final class Movie {
   final String overview;
   final String posterURL;
   final String backdropURL;
+  final String pageURL;
 
   const Movie({
     required this.id,
@@ -11,20 +12,17 @@ final class Movie {
     required this.overview,
     required this.posterURL,
     required this.backdropURL,
+    required this.pageURL,
   });
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      id: json['id'],
-      title: json['title'],
-      overview: json['overview'],
-      posterURL: "https://image.tmdb.org/t/p/w500${json['poster_path']}",
-      backdropURL: "https://image.tmdb.org/t/p/w500${json['backdrop_path']}",
+      id: json['id']!,
+      title: json['title']!,
+      overview: json['overview']!,
+      posterURL: "https://image.tmdb.org/t/p/w500${json['poster_path']!}",
+      backdropURL: "https://image.tmdb.org/t/p/w500${json['backdrop_path']!}",
+      pageURL: json['homepage']!,
     );
-  }
-
-  @override
-  String toString() {
-    return "title: $title\noverview: $overview";
   }
 }
