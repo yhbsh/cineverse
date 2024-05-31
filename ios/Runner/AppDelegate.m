80541f6f0d8fcad1d2f9dfc98b9aaee1676b70de
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-05-31 21:03:21 +0300

    use objc for iOS project, use exact versions for packages

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -4,8 +4,8 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
